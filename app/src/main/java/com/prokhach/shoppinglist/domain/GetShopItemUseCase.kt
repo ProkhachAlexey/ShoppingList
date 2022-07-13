338bebc7f1900d881db421f7bf004f6f83128576
Alexey Prokhach <asprokhach68@gmail.com>
2022-07-13 19:33:49 +0300

    Adding Model and UseCases

---
+++ app/src/main/java/com/prokhach/shoppinglist/domain/GetShopItemUseCase.kt
@@ -0,0 +1,9 @@
+package com.prokhach.shoppinglist.domain
+
+// Интерактор для получения элемента из списка покупок
+
+class GetShopItemUseCase {
+    fun getShopItem(shopItemId: Int): ShopItem {
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/prokhach/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,9 @@
+package com.prokhach.shoppinglist.domain
+
+// Интерактор для получения всего списка покупок
+
+class GetShopListUseCase {
+    fun getShopList(): List<ShopItem> {
+        TODO()
+    }
+}
