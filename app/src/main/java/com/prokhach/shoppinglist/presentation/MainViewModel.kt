b2277897c22da0aed264ed66c00ebba04707f640
Alexey Prokhach <asprokhach68@gmail.com>
2022-07-15 23:06:47 +0300

    Auto update implementation LiveData in UseCase function in ViewModel

--- app/src/main/java/com/prokhach/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/prokhach/shoppinglist/presentation/MainViewModel.kt
@@ -12,25 +12,19 @@ class MainViewModel : ViewModel() {
 
     private val repository = ShopListRepositoryImpl
 
-    val shopList = MutableLiveData<List<ShopItem>>()
-
     private val getShopListUseCase = GetShopListUseCase(repository)
+
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    fun getShopList() {
-        val list = getShopListUseCase.getShopList()
-        shopList.value = list
-    }
+    val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem) {
         deleteShopItemUseCase.deleteShopItem(shopItem)
-        getShopList()
     }
 
     fun changeEnableState(shopItem: ShopItem) {
         val newItem = shopItem.copy(enabled = !shopItem.enabled)
         editShopItemUseCase.editShopItem(newItem)
-        getShopList()
     }
 }
